d7843406eb06973887c29eda15da07492dac37d4
Tejo-TV <150041189+Tejo-TV@users.noreply.github.com>
2026-01-28 09:43:58 +0100

    UPDATE: Main styles
    main styles zijn toegevoegd

---
+++ Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="shortcut icon" type="x-icon" href="assets/Images/centrum-duurzaam-icon.png">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    
+</body>
+</html>
