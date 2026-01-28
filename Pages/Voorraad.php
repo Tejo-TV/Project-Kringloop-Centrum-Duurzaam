ec5dcc9602311e47595a58dab616e52cbb0ba661
Thimo kamp <6036553@mborijnland.nl>
2026-01-28 13:41:28 +0100

    Update Voorraad.php

--- Pages/Voorraad.php
+++ Pages/Voorraad.php
@@ -1,15 +1,23 @@
 <?php
+//---------------------------------------------------------------------------------------------------//
+// Naam script       : Voorraad.php
+// Omschrijving      : Dit is de voorraad beheer pagina
+// Naam ontwikkelaar : Tejo Veldman
+// Project           : Kringloop Centrum Duurzaam
+// Datum             : 28-01-2026
+//---------------------------------------------------------------------------------------------------// 
 session_start();
+
+if (!isset($_SESSION["userid"])) {
+    header("Location: ../login.php?error=notLoggedIn");
+    exit();
+}
+
 require_once '../../Config/database.php';
 require_once 'components/functions.inc.php';
 
 $db = new Database();
 $conn = $db->getConnection();
-
-if (!isset($_SESSION["userid"])) {
-    header("Location: login.php?error=notLoggedIn");
-    exit();
-}
 
 // Verwerk formulieren
 if (isset($_POST["voorraadToevoegen"])) {
