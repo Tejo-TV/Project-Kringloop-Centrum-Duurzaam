3a01b060c955b49164f8f2cdf31ecba8242bcefa
Suzanne Boon <6035697@mborijnland.nl>
2026-01-28 13:07:51 +0100

    CREATE: makeuser file

---
+++ Pages/make_user.php
@@ -0,0 +1,30 @@
+<?php
+//---------------------------------------------------------------------------------------------------//
+// Naam script       : make_user.php
+// Omschrijving      : Dit is de make user pagina van het Kringloop Centrum Duurzaam
+// Naam ontwikkelaar : Suzanne Boon
+// Project           : Kringloop Centrum Duurzaam
+// Datum             : 28-01-2026
+//---------------------------------------------------------------------------------------------------// 
+session_start();
+
+// Als je niet bent ingelogd, stuur je terug naar de login pagina anders popup.
+if (isset($_SESSION["userid"])) {
+    echo "<div class='popup2'> You are logged in. </div>";
+} else {
+    header("Location: login.php?error=notLoggedIn");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
